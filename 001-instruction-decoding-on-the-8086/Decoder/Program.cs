64885b1a7052f4600b443c95612e2870e9619ebb
mosukiton <harmohan.ss@live.co.uk>
2024-05-06 13:23:57 +0100

    completed homework001

---
+++ 001-instruction-decoding-on-the-8086/Decoder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Homework001;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length == 0) throw new ArgumentOutOfRangeException();
+
+        Reader reader = new(args[0]);
+        List<string> result = reader.ReadFile();
+
+        if (args.Length == 2)
+        {
+            File.WriteAllLines(args[1], result);
+        }
+        result.ForEach(x => Console.WriteLine(x));
+    }
+}
+
